1bec5ea839fbedc7f0512a3ff5871c1d2224d8ca
tom choi <qjatn0545@naver.com>
2024-09-26 16:22:26 +0900

    R2-S2

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,18 @@
 
 import Foundation
 
-print("Hello, World!")
+func picLottoNum() -> Int {
+    return Int.random(in: 1...45)
+}
 
+func showLottoNum() -> Set<Int> {
+    var lottoNums : Set<Int> = Set()
+
+    while lottoNums.count < 6 {
+        lottoNums.insert(picLottoNum())
+    }
+    
+    return lottoNums
+}
+
+print("당첨 번호 : \(showLottoNum())")
