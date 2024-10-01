4c6c98da6aa130f8b1cdac165312fa2c7ee749a8
jeseoyoung <jeseoyoung@seoyoung-MacBook-Pro.local>
2024-10-01 15:49:43 +0900

    refactor: checkLotto() 코드 수정

--- CodeStarterCamp_Week2/step3.swift
+++ CodeStarterCamp_Week2/step3.swift
@@ -7,20 +7,20 @@
 
 import Foundation
 
-var history: Dictionary = [String?: Set<Int>]()
-var times: Int = 1
+var history: Dictionary = [Int: Set<Int>]()
+var round: Int = 1
 
 func saveLotto(to lottoNumbers: Set<Int>) {
-    history["\(times)회차"] = lottoNumbers
-    times += 1
+    history[round] = lottoNumbers
+    round += 1
 }
 
 func checkLotto(to round: Int) {
-    if let lottoNumbers = history["\(round)회차"] {
-        let sortedNumbers: [Int] = lottoNumbers.sorted()
-        let numbersInRound = sortedNumbers.map { String($0) }.joined(separator: ", ")
-               print("\(round)회차의 로또 당첨 번호는 \(numbersInRound)입니다.")
-    } else {
+    guard let lottoNumbers = history[round] else {
         print("해당 회차의 정보가 존재하지 않습니다.")
+        return
     }
+    let sortedNumbers = lottoNumbers.sorted()
+        let numbersInRound = sortedNumbers.map { String($0) }.joined(separator: ", ")
+        print("\(round)회차의 로또 당첨 번호는 \(numbersInRound)입니다.")
 }
